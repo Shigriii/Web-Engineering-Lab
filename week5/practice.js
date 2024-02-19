fe4529c17be43671b921a9410257b052f95f9aa6
Shigriii <75520988+Shigriii@users.noreply.github.com>
2024-02-19 17:00:20 +0500

    Add files via upload

---
+++ week5/practice.js
@@ -0,0 +1,64 @@
+//Array
+/*const arr = ['Hello','world'];
+console.log(arr)
+arr.push('IBA')
+console.log(arr)
+console.log(arr.shift())
+console.log(arr.unshift('Unshift'))
+console.log(arr.includes('World'))
+
+//Object
+const person = {firstName:"John", lastName:"Doe", age:46};
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person)[0])
+console.log(Object.assign({}, person))
+console.log(Object.getOwnPropertyNames(person))
+Object.freeze(person)
+*/
+
+//Function
+/*function greet(name){
+    console.log('Hello '+name+'!')
+}
+
+//greet('Mushahid')
+
+const greet1 = (name)=>{
+    return 'hello, '+name+'!'
+};
+
+greet1('Mushahid')
+
+(function(){
+    console.log('I am immediately invoked')
+})();
+*/
+
+//double the array
+
+/*function doublearray(arr){
+    let arrayelemnt = [];
+    for(let i = 0; i<arr.length; i++){
+        arrayelemnt.push(arr[i]*2);
+        
+    }
+    return arrayelemnt;
+}
+
+arr = [1,2,3,4,5,6];
+double = doublearray(arr);
+console.log(double)
+*/
+//string
+
+/*const firstname = 'Mushahid'
+const middlename = 'Hussain'
+const lastname = 'Sarwar'
+
+console.log(firstname.length)
+console.log(firstname.charAt(0))
+console.log(firstname.charCodeAt(0))
+const fullname = firstname.concat(' ',middlename,' ',lastname)
+console.log(fullname)
+*/
